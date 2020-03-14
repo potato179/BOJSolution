4b8f0434289043040d99a99f2afc0f015fb60d3d
potato179 <44293278+potato179@users.noreply.github.com>
2020-03-14 19:17:29 +0900

    Create 1001.java

---
+++ 1001/1001.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] a){
+        Scanner s=new Scanner(System.in);
+        System.out.println(s.nextInt()-s.nextInt());
+    }
+}
