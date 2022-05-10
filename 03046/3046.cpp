b0dab65d3c0a54c27c7f89e390afdbcf8c2882c0
홍은표 <44293278+potato179@users.noreply.github.com>
2022-05-10 10:38:01 +0900

    5522

---
+++ 03046/3046.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int r1, s;
+    scanf("%d %d", &r1, &s);
+    printf("%d", s*2-r1);
+}
---
+++ 05522/5522.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int i1, i2, i3, i4, i5;
+    scanf("%d %d %d %d %d", &i1, &i2, &i3, &i4, &i5);
+    printf("%d", i1+i2+i3+i4+i5);
+}
