793731e449be433067a7edc0e91dcc92de30e3ba
홍은표 <44293278+potato179@users.noreply.github.com>
2022-05-10 10:49:16 +0900

    8370

---
+++ 06749/6749.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int y, m;
+    scanf("%d %d", &y, &m);
+    printf("%d", m+m-y);
+}
---
+++ 08370/8370.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int n1, n2, k1, k2;
+    scanf("%d %d %d %d", &n1, &n2, &k1, &k2);
+    printf("%d", n1*n2 + k1*k2);
+}
