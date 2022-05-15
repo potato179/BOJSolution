5b07d3d272742442b57ab4c442f67647292ff17f
홍은표 <44293278+potato179@users.noreply.github.com>
2022-05-15 09:28:49 +0900

    Update 01475.cpp

--- 01475/01475.cpp
+++ 01475/01475.cpp
@@ -1,7 +1,7 @@
 #include<cstdio>
 int a[10], x, r;
 int main(){
-    while(~scanf("%1d",&x)) a[x]++;
+    while(~scanf("%1d", &x)) a[x]++;
     a[6] = (a[6]+a[9]+1)/2;
     for (int i=1; i<9; i++) if(a[r] < a[i]) r = i;
     printf("%d", a[r]);
