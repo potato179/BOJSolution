7af90941188ebb29149bd9eae17e3f30d63912ca
홍은표 <44293278+potato179@users.noreply.github.com>
2022-05-15 09:28:40 +0900

    Update 01475.cpp

--- 01475/01475.cpp
+++ 01475/01475.cpp
@@ -4,5 +4,5 @@ int main(){
     while(~scanf("%1d",&x)) a[x]++;
     a[6] = (a[6]+a[9]+1)/2;
     for (int i=1; i<9; i++) if(a[r] < a[i]) r = i;
-    rintf("%d", a[r]);
-    }
+    printf("%d", a[r]);
+}
