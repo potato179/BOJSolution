45dcd022e066758c6cc5b139e3ee735de5253d5d
홍은표 <44293278+potato179@users.noreply.github.com>
2022-05-18 17:35:17 +0900

    4299

--- 04299/4299.py
+++ 04299/4299.py
@@ -1,6 +1,5 @@
 a, b = map(int, input().split())
-if a+b < 0 or a-b < 0 or (a+b)%2 != 0: print(-1)
+if a+b < 0 or a-b < 0 or (a+b)%2:
+    print(-1)
 else:
-    x = (a+b)//2
-    y = a-b
-    print(max(x, y), min(x, y)) 
+    print(max((a+b)/2, a-b), min((a+b)/2, a-b))
