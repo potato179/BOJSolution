ce060e13ab00bde0860024b3cf7bb9d5d4a09bb9
홍은표 <44293278+potato179@users.noreply.github.com>
2022-05-20 15:05:35 +0900

    Create 2443.cpp

---
+++ 02443/2443.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+	int input;
+	scanf("%d", &input);
+	for (int i = input; i > 0; i--) {
+		for (int j = input-i; j > 0; j--) printf(" ");
+		for (int k = 2*i-1; k > 0; k--) printf("*");
+		printf("\n");
+	}
+}
