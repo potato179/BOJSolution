7fc152b1f5762b80f0d928d15fddc28272b8821a
홍은표 <44293278+potato179@users.noreply.github.com>
2022-05-20 18:05:11 +0900

    15639

---
+++ 15639/15639.py
@@ -0,0 +1,5 @@
+c = input()
+if c == "Give you up" or c == "Let you down" or c == "Run around and desert you" or c == "Make you cry" or c == "Say goodbye" or c == "Tell a lie and hurt you":
+    print("NO")
+else:
+    print("YES")
